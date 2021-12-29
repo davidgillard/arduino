75da6d5ddced2fc45f6f728281c915c2479a1768
David GILLARD <david.gillard@ac-lille.fr>
2021-12-29 15:38:00 +0100

    ajout des données de platformio

---
+++ arduino-platformio/src/main.cpp
@@ -0,0 +1,30 @@
+/*
+  fichier : fun-mooc-Blink.cpp
+  auteur : David GILLARD
+  github: https://github.com/davidgillard
+
+  Clignotement
+  Allume la LED 13 pendant 5 seconde,
+  puis l'éteint pendant 5 seconde.
+
+Complément d'information ici on utilise le célébre language c++ pour faire 
+fonctionner le projet Blink pour ce faire :
+- Ajouter la librairie qui va bien :  #include <Arduino.h> au début du fichier source
+- Declaré chaque function personnalisé (excluding built-in, such as setup and loop) before it will be called.
+
+*/
+
+#include <Arduino.h>
+
+void someFunction(int num);
+
+void setup () {
+    someFunction(13);
+}
+
+void loop() {
+    delay(5000);
+}
+
+void someFunction(int num) {
+}
